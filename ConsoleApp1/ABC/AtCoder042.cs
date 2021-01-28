b4b50bcaadb958ae74662081e1405e4ee320a55f
Themyscira <takeda.syouhei@gmail.com>
2021-01-28 12:43:08 +0900

    Makes AtCoder042_a

--- ConsoleApp1/ABC/AtCoder042.cs
+++ ConsoleApp1/ABC/AtCoder042.cs
@@ -8,5 +8,25 @@ namespace ConsoleApp1.ABC
 {
     class AtCoder042
     {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            var word = Console.ReadLine().Split(' ');
+            A = int.Parse(word[0]);
+            B = int.Parse(word[1]);
+            C = int.Parse(word[2]);
+
+            if
+               (A + B + C == 17)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Console.ReadKey();
+        }
     }
 }
