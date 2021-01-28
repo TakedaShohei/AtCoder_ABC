8f6b75086626642f7dff9248d50b6aeca1be6df5
Themyscira <takeda.syouhei@gmail.com>
2021-01-28 12:54:55 +0900

    RemakeConsole

---
+++ ConsoleApp1/ABC/AtCoder086_A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.ABC
+{
+    class AtCoder086_A
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            var word = Console.ReadLine().Split(' ');
+            A = int.Parse(word[0]);
+            B = int.Parse(word[1]);
+            C = int.Parse(word[2]);
+
+            if
+               (A + B + C == 17)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
