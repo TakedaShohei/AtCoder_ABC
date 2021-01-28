f80b26dd939a08328d6b1d02de9263a42dac0bbb
Themyscira <takeda.syouhei@gmail.com>
2021-01-28 13:13:33 +0900

    UpgradeConsole

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/ABCBeginner/086A.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.ABCBeginner
+{
+    class _086A
+    {
+        private static void Select(string[] args)
+        {
+            int A, B, C;
+            var word = System.Console.ReadLine().Split(' ');
+            A = int.Parse(word[0]);
+            B = int.Parse(word[1]);
+            C = int.Parse(word[2]);
+
+            if
+               (A + B + C == 17)
+            {
+                System.Console.WriteLine("YES");
+            }
+            else
+            {
+                System.Console.WriteLine("NO");
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
