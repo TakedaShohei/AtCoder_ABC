4fbe650f001f7ea0627c41bfcfecdd71b3c97001
Themyscira <takeda.syouhei@gmail.com>
2021-01-28 17:47:46 +0900

    Slove 082_a

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/ABCBeginner/example.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Linq;
+namespace ConsoleApp1.ABCBeginner
+{
+    class example
+    {
+        private static void Main(string[] args)
+        {
+            int a, b;
+            
+            var input_ = Console.ReadLine().Split(' ');
+            a = int.Parse(input_[0]);
+            b = int.Parse(input_[1]);
+
+            
+            if ((a*b)%2 == 0)
+            {
+                System.Console.WriteLine("Even");
+            }
+            else
+            {
+                System.Console.WriteLine("Odd");
+            }
+
+        }
+    }
+}
